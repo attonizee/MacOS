d54a6561cc5c40295cfa83309d174173429b0db9
Anatolii Veselyi <anatoliiv1990@gmail.com>
2021-03-24 22:12:46 +0200

    script for default soft

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#add cask repo
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null ; brew install caskroom/cask/brew-cask 2> /dev/null
+#install Keka
+brew install --cask keka
+#install Mounty
+brew install --cask mounty
+#install TeamViewer
+brew install --cask teamviewer
+#install Skype
+brew install --cask skype
+#install Google Chrome
+brew install --cask google-chrome
