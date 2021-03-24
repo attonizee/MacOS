914ebbe1747c3d3ee4cf8677f267b84fe84d989d
Anatolii Veselyi <anatoliiv1990@gmail.com>
2021-03-24 23:35:19 +0200

    added macos update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+#update MacOS
+softwareupdate -i -a
+#install Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 #add cask repo
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null ; brew install caskroom/cask/brew-cask 2> /dev/null
